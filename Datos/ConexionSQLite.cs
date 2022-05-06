88e101ea67e9a989e5d24dccfdc75d7e2206d28d
Crhispo <cristianandrade2001@gmail.com>
2022-05-06 18:27:53 -0500

    Se agrego la crud y ejecucion hacia la base de datos

--- Datos/ConexionSQLite.cs
+++ Datos/ConexionSQLite.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Data.SQLite;
 using System.Data;
+using System.Data.SQLite;
 
 namespace Datos
 {
     public class ConexionSQLite
     {
-        static readonly string connectionString = @"Data Source=C:\Users\Crhispo\source\repos\VentaForm\Datos\Puntodeventa.sqlite;";
+        static readonly string connectionString = @"Data Source=.\Puntodeventa.sqlite;";
         readonly SQLiteConnection SQLite = new SQLiteConnection(connectionString);
         public int ConsultaLogin(string Usuario, string Clave)
         {
@@ -19,6 +19,71 @@ namespace Datos
             SQLite.Close();
 
             return count;
+        }
+
+        public int InsertarUsuario(string nombre, string apellidos, string dni, string telefono, string usuario, string clave)
+        {
+            int flag = 0;
+            SQLite.Open();
+            string query = $"Insert into Tbl_Persona (nombre,apellidos,dni,telefono,usuario,clave) values ('{nombre}','{apellidos}',{dni},{telefono},'{usuario}','{clave}')";
+            SQLiteCommand cmd = new SQLiteCommand(query, SQLite);
+            flag = cmd.ExecuteNonQuery();
+            SQLite.Close();
+            return flag;
+        }
+
+        public int ModificarUsuario(string nombre, string apellidos, string dni, string telefono, string usuario, string clave)
+        {
+            int flag = 0;
+            string StringUserId;
+            SQLite.Open();
+            string queryid = $"Select * from Tbl_Persona where dni = {dni}";
+            SQLiteCommand cmdid = new SQLiteCommand(queryid, SQLite);
+            SQLiteDataReader Usuario = cmdid.ExecuteReader();
+            if (Usuario.Read())
+            {
+                StringUserId = Usuario["id"].ToString();
+                string query = $"Update Tbl_Persona set nombre = '{nombre}', apellidos = '{apellidos}', dni = {dni}, telefono = {telefono}, usuario = '{usuario}', clave = '{clave}' where id = {StringUserId}";
+                SQLiteCommand cmd = new SQLiteCommand(query, SQLite);
+                flag = cmd.ExecuteNonQuery();
+                SQLite.Close();
+                return flag;
+            }
+            else
+            {
+                SQLite.Close();
+                return flag;
+            }
+
+
+
+        }
+
+        public int EliminarUsuario(string dni)
+        {
+            int flag = 0;
+            string StringUserId;
+            SQLite.Open();
+            string queryid = $"Select * from Tbl_Persona where dni = {dni}";
+            SQLiteCommand cmdid = new SQLiteCommand(queryid, SQLite);
+            SQLiteDataReader Usuario = cmdid.ExecuteReader();
+            if (Usuario.Read())
+            {
+                StringUserId = Usuario["id"].ToString();
+                string query = $"delete from Tbl_Persona where id = {StringUserId}";
+                SQLiteCommand cmd = new SQLiteCommand(query, SQLite);
+                flag = cmd.ExecuteNonQuery();
+                SQLite.Close();
+                return flag;
+            }
+            else
+            {
+                SQLite.Close();
+                return flag;
+            }
+
+
+
         }
 
         public DataTable ConsultarUsuarios()
