cfb2fb10100f10b883cab5ab4c3707fa5fead2e6
Crhispo <cristianandrade2001@gmail.com>
2022-05-09 17:30:53 -0500

    Se creo una capa paraseparar la conexion de las connsultas

--- Datos/ConexionSQLite.cs
+++ Datos/ConexionSQLite.cs
@@ -1,100 +1,52 @@
 ﻿using System;
 using System.Data;
 using System.Data.SQLite;
-
 namespace Datos
 {
     public class ConexionSQLite
     {
+        int flag = 0;
         static readonly string connectionString = @"Data Source=.\Puntodeventa.sqlite;";
         readonly SQLiteConnection SQLite = new SQLiteConnection(connectionString);
-        public int ConsultaLogin(string Usuario, string Clave)
+        public int QuerySQLite(string Query, int TipoQuery)
         {
-            int count;
+            int resp;
             SQLite.Open();
-            string Query = "Select Count(*) From Tbl_Persona where usuario ='" + Usuario + "'" +
-                "and Clave ='" + Clave + "'";
             SQLiteCommand cmd = new SQLiteCommand(Query, SQLite);
-            count = Convert.ToInt32(cmd.ExecuteScalar());
+            /*
+            Codigo numerico tipo de Query
+            Consulta Login = 0
+            Modificar datos de tabla = 1
+            Consultar id = 2
+            Consuta tabla adapter = 3
+            */
+            switch (TipoQuery)
+            {
+                case 0:
+                    int count;
+                    count = Convert.ToInt32(cmd.ExecuteScalar());
+                    resp = count;
+                    break;
+                case 1:
+                    flag = cmd.ExecuteNonQuery();
+                    resp = flag;
+                    break;
+                default:
+                    resp = flag;
+                    break;
+            };
             SQLite.Close();
-
-            return count;
+            return resp;
         }
-
-        public int InsertarUsuario(string nombre, string apellidos, string dni, string telefono, string usuario, string clave)
+        public DataTable ConsultarUsers(string Query)
         {
-            int flag = 0;
             SQLite.Open();
-            string query = $"Insert into Tbl_Persona (nombre,apellidos,dni,telefono,usuario,clave) values ('{nombre}','{apellidos}',{dni},{telefono},'{usuario}','{clave}')";
-            SQLiteCommand cmd = new SQLiteCommand(query, SQLite);
-            flag = cmd.ExecuteNonQuery();
-            SQLite.Close();
-            return flag;
-        }
-
-        public int ModificarUsuario(string nombre, string apellidos, string dni, string telefono, string usuario, string clave)
-        {
-            int flag = 0;
-            string StringUserId;
-            SQLite.Open();
-            string queryid = $"Select * from Tbl_Persona where dni = {dni}";
-            SQLiteCommand cmdid = new SQLiteCommand(queryid, SQLite);
-            SQLiteDataReader Usuario = cmdid.ExecuteReader();
-            if (Usuario.Read())
-            {
-                StringUserId = Usuario["id"].ToString();
-                string query = $"Update Tbl_Persona set nombre = '{nombre}', apellidos = '{apellidos}', dni = {dni}, telefono = {telefono}, usuario = '{usuario}', clave = '{clave}' where id = {StringUserId}";
-                SQLiteCommand cmd = new SQLiteCommand(query, SQLite);
-                flag = cmd.ExecuteNonQuery();
-                SQLite.Close();
-                return flag;
-            }
-            else
-            {
-                SQLite.Close();
-                return flag;
-            }
-
-
-
-        }
-
-        public int EliminarUsuario(string dni)
-        {
-            int flag = 0;
-            string StringUserId;
-            SQLite.Open();
-            string queryid = $"Select * from Tbl_Persona where dni = {dni}";
-            SQLiteCommand cmdid = new SQLiteCommand(queryid, SQLite);
-            SQLiteDataReader Usuario = cmdid.ExecuteReader();
-            if (Usuario.Read())
-            {
-                StringUserId = Usuario["id"].ToString();
-                string query = $"delete from Tbl_Persona where id = {StringUserId}";
-                SQLiteCommand cmd = new SQLiteCommand(query, SQLite);
-                flag = cmd.ExecuteNonQuery();
-                SQLite.Close();
-                return flag;
-            }
-            else
-            {
-                SQLite.Close();
-                return flag;
-            }
-
-
-
-        }
-
-        public DataTable ConsultarUsuarios()
-        {
-            string query = "select * from Tbl_Persona";
-            SQLiteCommand cmd = new SQLiteCommand(query, SQLite);
+            SQLiteCommand cmd = new SQLiteCommand(Query, SQLite);
             SQLiteDataAdapter data = new SQLiteDataAdapter(cmd);
             DataTable table = new DataTable();
             data.Fill(table);
+            SQLite.Close();
             return table;
-
         }
     }
 }
---
+++ Datos/GestionSQLiteUsers.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+namespace Datos
+{
+    public class GestionSQLiteUsers
+    {
+        readonly ConexionSQLite conexionSQLite = new ConexionSQLite();
+        private int flag;
+        /*
+        Codigo numerico tipo de Query
+        Consulta Login = 0
+        Modificar datos de tabla = 1
+        Consultar id = 2
+        Consuta tabla adapter = 3
+        */
+        public int ConsultaLogin(string Usuario, string Clave)
+        {
+            string Query = $"Select Count(*) From Tbl_Persona where usuario = '{Usuario}' and Clave = '{Clave}'";
+            int count = conexionSQLite.QuerySQLite(Query, 0);
+            return count;
+        }
+        public int InsertarUsuario(string nombre, string apellidos, string dni, string telefono, string usuario, string clave)
+        {
+            string Query = $"Insert into Tbl_Persona (nombre, apellidos, dni, telefono, usuario, clave) values ('{nombre}', '{apellidos}', {dni}, {telefono}, '{usuario}', '{clave}')";
+            flag = conexionSQLite.QuerySQLite(Query, 1);
+            return flag;
+        }
+        public int ModificarUsuario(string nombre, string apellidos, string dni, string telefono, string usuario, string clave)
+        {
+            string Query = $"Update Tbl_Persona set nombre = '{nombre}', apellidos = '{apellidos}', dni = {dni}, telefono = {telefono}, usuario = '{usuario}', clave = '{clave}' where dni = {dni}";
+            flag = conexionSQLite.QuerySQLite(Query, 1);
+            return flag;
+        }
+        public int EliminarUsuario(string dni)
+        {
+            string Query = $"delete from Tbl_Persona where dni = {dni}";
+            flag = conexionSQLite.QuerySQLite(Query, 1);
+            return flag;
+        }
+        public DataTable ConsultarUsuarios()
+        {
+            string Query = "select * from Tbl_Persona";
+            DataTable table = conexionSQLite.ConsultarUsers(Query);
+            return table;
+        }
+    }
+}
